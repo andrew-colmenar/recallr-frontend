82274f3d5a0e73fd243b33a31f22556e13db1f6b
andrew-colmenar <andrewgcolmenar@gmail.com>
2025-06-01 14:13:05 -0700

    hmm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar-container">
-      {/* Placeholder for logo and navigation links */}
-      <span>Recallr AI</span>
-      <div>
+      <div className="navbar-left">
+        <img src={logo} alt="Recallr AI Logo" className="logo" />
+        <span>Recallr AI</span>
+      </div>
+      <div className="navbar-right">
         <a href="#">Dashboard</a>
         <a href="#">Playground</a>
         <a href="#">Docs</a>
